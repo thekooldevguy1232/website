87b030a8f5b64dd931eb4af77980b4967af875a2
thekooldevguy1232 <nkpradeep80@gmail.com>
2020-10-16 17:39:15 +0530

    changes

--- server.js
+++
@@ -1,7 +0,0 @@
-//let colors = [‘#9be9a8’, ‘#216e39’, ’#FFFFFF’];
-let colors = ['green','orange','yellow'];
-
-document.querySelectorAll('rect')
-.forEach((item) => {
-     item.setAttribute('fill’,colors[Math.floor(Math.random()*3)]);
-     })
